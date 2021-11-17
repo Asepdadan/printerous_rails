a4d335123aa8f7a89225808085ff97a411707996
ASEP DADAN <dadanasep74@gmail.com>
2021-11-18 00:51:55 +0700

    menu user

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,4 +7,49 @@ class UserController < ApplicationController
     def index
 
     end
+
+    def new
+        @user = User.new
+    end
+   
+    def store
+        
+    end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+
+            redirect_to user_index_path, notice: 'Task berhasil ditambahkan'
+        else
+
+            render :new
+        end
+    end
+
+    def edit
+        @ambil_id = User.find(params[:id])
+    end
+    
+    def update
+        @ambil_id = User.find(params[:id])
+
+        if @ambil_id.update(user_params)
+            
+            redirect_to user_index_path, notice: 'Task berhasil diupdate'
+        else
+
+            render :edit
+        end
+    end
+
+    def get_user
+        render json: { data: User.all() }
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:name, :phone, :email, :password)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,48 +1,31 @@
 Rails.application.routes.draw do
-  get 'person/new'
-  get 'person/create'
+  # get 'person/new'
+  # get 'person/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # root "articles#index"
 
-  get "/articles", to: "articles#index", as: "article_index"
-  get "/articles/:id", to: "articles#show", as: "article"
+  # get "/articles", to: "articles#index", as: "article_index"
+  # get "/articles/:id", to: "articles#show", as: "article"
 
+  #organization
    resources :organization
+   get 'get_dataset', to: 'organization#get_dataset'
    post "organization", to: "organization#store", as: "organization_store"
    
-   
+   #person
    get "organization/person/:id", to: "person#new", as: "organization_new"
    post "organization/person", to: "person#store", as: "person_store"
    delete "person/:id", to: "person#delete", as: "person_delete"
 
+   #user
    resources :user
-   get 'get_dataset', to: 'organization#get_dataset'
-
+   get 'get_user', to: 'user#get_user'
+   post 'user/create', to: 'user#create'
+   
+   #auth
    get '/login', to: 'auth#form_login', as: 'form_login'
-  
    post '/login', to: 'auth#login', as: 'login_post'
-
-  
    root to: 'home#index', as: 'home_index'
-
-  
    get '/logout/:id', to: 'auth#logout', as: 'user_logout'
-
-  
-   scope '/v1/admin' do
-
-	
-    # localhost:3000/v1/admin/dashboard
-	
-    scope '/dashboard' do
-
-	    # localhost:3000/v1/admin/dashboard/
-	    get '/', to: 'admin_dashboard#index', as: 'admin_dashboard_index'
-	
-    end
-
-  
-  end
-
 
 end
